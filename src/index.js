25bfcda55a8fd757daead1317067a7f89d9cd5c7
Anastasiya <annabycreate@gmail.com>
2022-02-24 17:11:03 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {    
+    if (n < 0) {
+        let nn = n * (-1);
+        let str = String(nn);
+        let arr = str.split('');       
+        return Number(arr.reverse().join(''));      
+      
+    } else {    
+        let str = String(n);
+        let arr = str.split('');       
+        return Number(arr.reverse().join(''));        
+    }
 }
+
